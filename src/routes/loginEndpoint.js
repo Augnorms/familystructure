a31dbaf080f69f1724cbf932da3a4eea111e41c1
Augustine Normanyo <augustine.normanyo@amalitech.com>
2023-11-26 16:58:38 +0100

    feature: added a login feature

--- src/routes/loginEndpoint.js
+++ src/routes/loginEndpoint.js
@@ -1,5 +1,62 @@
 const express = require("express");
 const db = require("../dbconnect");
-const token = require("jsonwebtoken");
+const jwtoken = require("jsonwebtoken");
 const router = express.Router();
 const bycrypt = require("bcrypt");
+
+
+router.get("/", (req, res)=>{
+    const {username, password} = req.body;
+    
+    db.query('SELECT username, password FROM logins WHERE username = ?', 
+    [username], 
+    (err, resData)=>{
+       if(err){
+        res.status(401).json({
+          success:false,
+          code:401,
+          message:"Failed to login username, password incorrect"
+        });
+       }else if(resData.length === 0){
+        res.status(401).json({
+            success:false,
+            code:401,
+            message:"Failed to login username, password incorrect"
+          });
+       }else{
+         const user = resData[0]
+         const isPassword = bycrypt.compare(password, user.password);
+
+         if(isPassword){
+            const token = jwtoken.sign({ username: user.username }, "validate12345", {
+                expiresIn: "1h", 
+            });
+
+            db.query("SELECT loginId, password, email, firstname, lastname FROM logins Where username = ?", [username], (err, response)=>{
+                if(err){
+                    res.status(401).json({
+                        message:"Failed to fetch"
+                    });
+                }else{
+                    res.status(201).json({
+                        success:true,
+                        code:201,
+                        message:"login successful",
+                        token:token,
+                        data:response
+                    });
+                }
+            });
+         }else{
+            res.status(401).json({
+                success:false,
+                code:401,
+                message:"Failed to login username, password incorrect"
+              });
+         }
+       }
+    });
+
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,14 +3,16 @@ const cors = require("cors");
 const { json } = require("body-parser");
 const app = express();
 const createUser = require("./routes/createUser");
+const loginUser = require("./routes/loginEndpoint");
 
 app.use(express.json());
 
 //routers defined here
 app.use("/createuser", createUser);
+app.use("/login", loginUser);
 
 let port = 4000;
 
 app.listen(port, ()=>{
-    console.log(`app is running on port ${port}`);
+    console.log(`app is running on port ${port}`);  
 });
