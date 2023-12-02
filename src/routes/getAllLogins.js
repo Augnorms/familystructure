69a70b471f90ec452586193a922e1ab04f5acfe4
Augustine Normanyo <augustine.normanyo@amalitech.com>
2023-12-02 11:55:17 +0100

    added a getalllogin endpoint

---
+++ src/routes/getAllLogins.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const db = require("../dbconnect");
+const router = express.Router();
+
+router.get("/", (req, res)=>{
+    db.query("SELECT * FROM logins", (err, resdata)=>{
+       if(err){
+        res.status(401).json({
+            success:false,
+            code:401,
+            message:"Failed to fetch data"
+        })
+       }else if(resdata.length == 0){
+        res.status(401).json({
+            success:false,
+            code:401,
+            message:"Failed to fetch data"
+        })
+       }else{
+         res.status(201).json({
+            success:true,
+            code:201,
+            message:"data available",
+            data:resdata[0]
+         });
+       }
+    });
+});
+
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ const { json } = require("body-parser");
 const app = express();
 const createUser = require("./routes/createUser");
 const loginUser = require("./routes/loginEndpoint");
+const getalllogins = require("./routes/getAllLogins");
 
 app.use(express.json());
 
 //routers defined here
 app.use("/createuser", createUser);
 app.use("/login", loginUser);
+app.use("/getalllogins", getalllogins);
 
 let port = 4000;
 
