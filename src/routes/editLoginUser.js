c266b0ef4601ceb075e81196920990d402deb3d1
Augustine Normanyo <augustine.normanyo@amalitech.com>
2023-12-02 15:19:10 +0100

    update feature added

--- src/routes/editLoginUser.js
+++ src/routes/editLoginUser.js
@@ -3,8 +3,31 @@ const db = require("../dbconnect");
 const router = express.Router();
 
 router.put("/", (req, res)=>{
-    const {userid} = req.body;
-
+    const {userid, username, firstname, lastname, email, isadmin} = req.body;
+    
+    db.query("UPDATE logins SET username = ?, firstname = ?, lastname = ?, email = ?, isadmin = ? WHERE loginId = ? LIMIT 1",
+    [username, firstname, lastname, email, isadmin, userid],
+    (err, resdata)=>{
+      if(err){
+        res.status(401).json({
+            success:false,
+            code:401,
+            message:"Failed to update user "+ username+ " "+err.message
+        });
+      }else if(resdata.length === 0){
+        res.status(401).json({
+            success:false,
+            code:401,
+            message:"Failed to update user "+ username
+        }); 
+      }else{
+        res.status(201).json({
+            success:true,
+            code:201,
+            message:`user ${username} has been updated successfully`
+        });
+      }
+    });
     
 });
 
